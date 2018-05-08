c7641095c0530ed320222ea6431e4f2eac35b66a
Hector Flores <hecto932@gmail.com>
2018-05-08 18:23:06 -0500

    Soporte de fuentes en Webpack

---
+++ url-loader-fonts/src/js/index.js
@@ -0,0 +1,13 @@
+import '../css/estilos.css'
+import { firstMessage, delayedMessage } from './message'
+import platziImg from '../images/platzi.png'
+
+document.write(firstMessage)
+delayedMessage()
+
+const img = document.createElement('img')
+img.setAttribute('src', platziImg)
+img.setAttribute('width', 50)
+img.setAttribute('height', 50)
+document.body.append(img)
+console.log(`Hola mundo desde external.webpack.config!`)
