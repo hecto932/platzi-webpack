caf95948e993cd81b370988b108b803483d68d2d
Hector Flores <hecto932@gmail.com>
2018-05-09 18:05:45 -0500

    Estilos con Sass

---
+++ sass-loader/src/components/teachers.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import Teacher from './teacher'
+import '../css/teachers.scss'
+
+class Teachers extends Component {
+  render () {
+    return (
+      <div>
+        <ul className="Teachers">
+          {this.props.data.teachers.map((teacherData, index) => {
+            return <Teacher key={index} {...teacherData} />
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Teachers
