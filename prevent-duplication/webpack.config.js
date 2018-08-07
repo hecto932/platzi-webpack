60e771992c9f896844600bda93fe520b496ed818
Hector Flores <hecto932@gmail.com>
2018-08-07 15:27:13 -0500

    Prevenir codigo duplicado

---
+++ prevent-duplication/webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/js/index.js'),
+    contact: path.resolve(__dirname, 'src/js/contact.js')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      // Aquí van los loaders
+      {
+        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000, // bytes
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react']
+          }
+        }
+      },
+      {
+        // test - Que tipo de archivo quiero reconocer,
+        // use - Que loader se va encargar del archivo
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          // ['style-loader', 'css-loader'],
+          // fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+                importLoaders: 1
+              }
+            },
+            'postcss-loader'
+          ]
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            'css-loader',
+            {
+              loader: 'stylus-loader',
+              options: {
+                use: [
+                  require('nib'),
+                  require('rupture')
+                ],
+                import: [
+                  '~nib/lib/nib/index.styl',
+                  '~rupture/rupture/index.styl'
+                ]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', {
+            loader: 'less-loader',
+            options: {
+              noIeCompat: true
+            }
+          }]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('css/[name].css')
+  ],
+  optimization: {
+    splitChunks: {
+      name: 'common',
+      chunks: 'initial'
+    }
+  }
+}
